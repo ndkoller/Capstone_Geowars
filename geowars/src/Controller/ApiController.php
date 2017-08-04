0cd2ec0253423be9f4d47117c7d171c6213e889e
Geo Wars <ryan.mackey@gmail.com>
2017-08-04 13:11:59 +0000

    Added addjacent territory information

--- geowars/src/Controller/ApiController.php
+++ geowars/src/Controller/ApiController.php
@@ -95,7 +95,8 @@ class ApiController extends AppController
                 "points" => $mapInfo['points'][$i],
                 "color" => $colors[$territoryById[$i]->user_id],
                 "center" => $mapInfo['centers'][$i],
-                "troops" => $territoryById[$i]->num_troops
+                "troops" => $territoryById[$i]->num_troops,
+                "adjacentTerritories" => $mapInfo['$adjacentTerritories'][$i]
                 );
         }
         $game["map"] = $map;
@@ -120,6 +121,7 @@ class ApiController extends AppController
     public function getMapPoints() {
         $mapPoints = array();
         $mapPointCenters = array();
+        $adjacentTerritories = array();
         
         $mapPoints[0] = array( array('x' => 150, 'y' => 0), 
                         array( 'x' => 250, 'y' => 0),
@@ -128,12 +130,14 @@ class ApiController extends AppController
                         array( 'x' => 150, 'y' => 233.3333333334),
                         array( 'x'=> 100, 'y' => 116.666667));
         $mapPointCenters[0] = array( 'x' => 200, 'y' => 116.666667);
+        $adjacentTerritories[0] = array(1, 3, 4);
         
         $mapPoints[1] = array( array('x' => 250, 'y' => 0), 
                         array( 'x' => 450, 'y' => 0),
                         array( 'x' => 400, 'y' => 116.666666667),
                         array( 'x' => 300, 'y' => 116.666666667));
-        $mapPointCenters[1] = array( 'x' => 350, 'y' => 58.33334);                
+        $mapPointCenters[1] = array( 'x' => 350, 'y' => 58.33334);
+        $adjacentTerritories[1] = array(0, 2, 4);
         
         $mapPoints[2] = array( array('x' => 450, 'y' => 0), 
                         array( 'x' => 550, 'y' => 0),
@@ -142,22 +146,26 @@ class ApiController extends AppController
                         array( 'x' => 450, 'y' => 233.3333333334),
                         array( 'x'=> 400, 'y' => 116.666667));
         $mapPointCenters[2] = array( 'x' => 500, 'y' => 116.6666667);
+        $adjacentTerritories[2] = array(1, 4, 5);
         
         $mapPoints[3] = array( array('x' => 100, 'y' => 116.666667), 
                         array( 'x' => 50, 'y' => 233.33334),
                         array( 'x' => 150, 'y' => 233.333334));
         $mapPointCenters[3] = array( 'x' => 100, 'y' => 194);
+        $adjacentTerritories[3] = array(0, 6);
         
         $mapPoints[4] = array( array('x' => 300, 'y' => 116.66667), 
                         array( 'x' => 400, 'y' => 116.666667),
                         array( 'x' => 450, 'y' => 233.3333334),
                         array( 'x'=> 250, 'y' => 233.33334));
         $mapPointCenters[4] = array( 'x' => 350, 'y' => 175);
+        $adjacentTerritories[4] = array(0, 1, 2, 8);
         
         $mapPoints[5] = array( array('x' => 600, 'y' => 116.666667), 
                         array( 'x' => 550, 'y' => 233.33334),
                         array( 'x' => 650, 'y' => 233.333334));
         $mapPointCenters[5] = array( 'x' => 600, 'y' => 194);
+        $adjacentTerritories[5] = array(2, 13);
         
         $mapPoints[6] = array( array('x' => 50, 'y' => 233.333334), 
                         array( 'x' => 150, 'y' => 233.333334),
@@ -166,42 +174,49 @@ class ApiController extends AppController
                         array( 'x' => 50, 'y' => 466.6666667),
                         array( 'x'=> 0, 'y' => 350));
         $mapPointCenters[6] = array( 'x' => 100, 'y' => 350);
+        $adjacentTerritories[6] = array(3, 7, 11, 14);
         
         $mapPoints[7] = array( array('x' => 150, 'y' => 233.333334), 
                         array( 'x' => 250, 'y' => 233.33334),
                         array( 'x' => 300, 'y' => 350),
                         array( 'x'=> 200, 'y' => 350));
         $mapPointCenters[7] =array( 'x' => 225, 'y' => 291.666667);
+        $adjacentTerritories[7] = array(0, 6, 8, 11);
         
         $mapPoints[8] = array( array('x' => 250, 'y' => 233.333334), 
                         array( 'x' => 450, 'y' => 233.33334),
                         array( 'x' => 400, 'y' => 350),
                         array( 'x'=> 300, 'y' => 350));
         $mapPointCenters[8] = array( 'x' => 350, 'y' => 291.666667);
+        $adjacentTerritories[8] = array(4, 7, 9, 10);
         
         $mapPoints[9] = array( array('x' => 450, 'y' => 233.333334), 
                         array( 'x' => 550, 'y' => 233.3333334),
                         array( 'x' => 500, 'y' => 350),
                         array( 'x'=> 400, 'y' => 350));
         $mapPointCenters[9] = array( 'x' => 475, 'y' => 291.666667);
+        $adjacentTerritories[9] = array(2, 8, 12, 13);
         
         $mapPoints[10] = array( array('x' => 300, 'y' => 350), 
                         array( 'x' => 400, 'y' => 350),
                         array( 'x' => 450, 'y' => 466.666667),
                         array( 'x'=> 250, 'y' => 466.66667));
         $mapPointCenters[10] = array( 'x' => 350, 'y' => 408.333334);
+        $adjacentTerritories[10] = array(8, 11, 12, 17);
         
         $mapPoints[11] = array( array('x' => 200, 'y' => 350), 
                         array( 'x' => 300, 'y' => 350),
                         array( 'x' => 250, 'y' => 466.666667),
                         array( 'x'=> 150, 'y' => 466.66667));
         $mapPointCenters[11] = array( 'x' => 225, 'y' => 408.33334);
+        $adjacentTerritories[11] = array(6, 7, 10, 16);
         
         $mapPoints[12] = array( array('x' => 400, 'y' => 350), 
                         array( 'x' => 500, 'y' => 350),
                         array( 'x' => 550, 'y' => 466.66667),
                         array( 'x'=> 450, 'y' => 466.66667));
         $mapPointCenters[12] = array( 'x' => 475, 'y' => 408.33334);
+        $adjacentTerritories[12] = array(9, 10, 13, 19);
         
         $mapPoints[13] = array( array('x' => 550, 'y' => 233.333334), 
                         array( 'x' => 650, 'y' => 233.333334),
@@ -210,16 +225,19 @@ class ApiController extends AppController
                         array( 'x' => 550, 'y' => 466.6666667),
                         array( 'x'=> 500, 'y' => 350));
         $mapPointCenters[13] = array( 'x' => 600, 'y' => 350);
+        $adjacentTerritories[13] = array(5, 9, 12, 15);
         
         $mapPoints[14] = array( array('x' => 150, 'y' => 466.6666667),
                       array('x' => 50, 'y' =>  466.6666667),
                       array('x' => 100, 'y' => 583.333334));
         $mapPointCenters[14] = array('x' => 100, 'y' => 505.5555556);
+        $adjacentTerritories[14] = array(6, 16);
         
         $mapPoints[15] = array( array('x' => 550, 'y' => 466.6666667),
                        array('x' => 650, 'y' => 466.6666667),
                        array('x' => 600, 'y' => 583.333334));
         $mapPointCenters[15] = array('x' => 600, 'y' => 505.5555556);
+        $adjacentTerritories[15] = array(13, 19);
         
         $mapPoints[16] = array(array('x' => 150, 'y' => 466.6666667),
                         array('x' => 250, 'y' => 466.6666667),
@@ -228,18 +246,21 @@ class ApiController extends AppController
                         array('x' => 150, 'y' => 700),
                         array('x' => 100, 'y' => 583.3333334));
         $mapPointCenters[16] = array('x' => 200, 'y' => 583.3333334);
+        $adjacentTerritories[16] = array(11, 14, 17, 18);
         
         $mapPoints[17] = array( array('x' => 250, 'y' => 466.6666667),
                           array('x' => 450, 'y' => 466.6666667),
                           array('x' => 400, 'y' => 583.3333334),
                           array('x' => 300, 'y' => 583.3333334));
         $mapPointCenters[17] = array('x' => 350, 'y' => 525);
+        $adjacentTerritories[17] = array(10, 16, 18, 19);
         
         $mapPoints[18] = array( array('x' => 300, 'y' => 583.3333334),
                         array('x' => 400, 'y' => 583.3333334),
                         array('x' => 450, 'y' => 700),
                         array('x' => 250, 'y' => 700));
         $mapPointCenters[18] = array('x' => 350, 'y' => 641.6666667);
+        $adjacentTerritories[18] = array(16, 17, 19);
         
         $mapPoints[19] = array(array('x' => 450, 'y' => 466.6666667),
                         array('x' => 550, 'y' => 466.6666667),
@@ -248,10 +269,12 @@ class ApiController extends AppController
                         array('x' => 450, 'y' => 700),
                         array('x' => 400, 'y' => 583.3333334));
         $mapPointCenters[19] = array('x' => 500, 'y' => 583.3333334);
+        $adjacentTerritories[19] = array(12, 15, 17, 18);
         
         $mapInfo = array();
         $mapInfo['points'] = $mapPoints;
         $mapInfo['centers'] = $mapPointCenters;
+        $mapInfo['$adjacentTerritories'] = $adjacentTerritories;
         
         return $mapInfo;
     }
